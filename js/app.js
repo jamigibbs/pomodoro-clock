64061ba5423b7e8bb9ff6be23e041f8e03b45dd8
Jami Gibbs <jami0821@gmail.com>
2016-04-08 15:38:08 -0500

    Adding basic timer functionality

--- js/app.js
+++ js/app.js
@@ -1,5 +1,72 @@
 (function(){
   'use strict';
 
+  $(document).ready(function(){
+
+    document.getElementById('pause').classList.add('hidden');
+
+    // Define our variables
+    var breakLength = 5,
+        sessionLength = 25,
+        tick,
+        minutes,
+        seconds;
+
+    // The brains
+    var timerObj = {
+
+      countdown: function(duration){
+        var timer = duration * 60;
+
+        tick = setInterval(function () {
+          minutes = parseInt(timer / 60, 10);
+          seconds = parseInt(timer % 60, 10);
+
+          minutes = minutes < 10 ? "0" + minutes : minutes;
+          seconds = seconds < 10 ? "0" + seconds : seconds;
+
+          $('#minutes').text(minutes);
+          $('#seconds').text(seconds);
+
+          if (--timer < 0) {
+            timer = duration;
+            $('#time').text('Break time!');
+            timerObj.pause();
+          }
+        }, 1000);
+      },
+
+      pause: function(){
+        clearInterval(tick);
+        sessionLength = minutes + (seconds / 60);
+      }
+
+    };
+
+    // Start timer
+    $('#start').click(function(){
+      timerObj.countdown(sessionLength);
+      document.getElementById('start').classList.add('hidden');
+      document.getElementById('pause').classList.remove('hidden');
+    });
+
+    // Pause timer
+    $('#pause').click(function(){
+      timerObj.pause();
+      document.getElementById('start').classList.remove('hidden');
+      document.getElementById('pause').classList.add('hidden');
+    });
+
+    // Reset timer
+    $('#reset').click(function(){
+      timerObj.pause();
+      sessionLength = 25;
+      $('#minutes').text(sessionLength);
+      $('#seconds').text('00');
+      document.getElementById('start').classList.remove('hidden');
+      document.getElementById('pause').classList.add('hidden');
+    });
+
+  });
 
 }());
