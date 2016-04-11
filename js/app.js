81c54c68d4a4febe85d96ae887716026f176a4e7
Jami Gibbs <jami0821@gmail.com>
2016-04-11 14:57:07 -0500

    Adding notations

--- js/app.js
+++ js/app.js
@@ -24,8 +24,9 @@
 
       // Countdown functionality
       countdown: function(duration){
-        var timer = duration * 60;
+        var timer = 0.1 * 60;
         active = true;
+
         // Assigning to var so we can use clearInterval when paused
         tick = setInterval(function () {
           minutes = parseInt(timer / 60, 10);
@@ -34,15 +35,19 @@
           minutes = minutes < 10 ? "0" + minutes : minutes;
           seconds = seconds < 10 ? "0" + seconds : seconds;
 
+          // Display the min/secs
           $('#minutes').text(minutes);
           $('#seconds').text(seconds);
 
           // When the timer completes
           if (--timer < 0) {
-            timer = duration;
+            // Keep track of the timer's status
             active = false;
+            // Stops the interval
             timerBrain.pause();
+            // Swap between work and break sessions
             sessionSwitch();
+            // Play the ding sound
             ding.play();
           }
 
@@ -51,7 +56,9 @@
 
       // Pause functionality
       pause: function(){
+        // Stops the setInterval
         clearInterval(tick);
+        // Calculates the current session time for restarting
         sessionLength = minutes + (seconds / 60);
       }
 
@@ -59,25 +66,33 @@
 
     function sessionSwitch(){
       if(breakTime){
-        // Switch to work session
+        // Change to work session if a break time finished
         breakTime = false;
+        // Grab the work session time that the user wants
         sessionLength = document.getElementById('session-length').innerHTML;
 
+        // Display work time
         $('#minutes').text(sessionLength);
         $('#seconds').text('00');
-        $('#notice').text('Work time!');
+        // Announce a work time will begin next
+        $('#notice').text('Work time');
 
+        // Show the start button & hide the pause button
         document.getElementById('start').classList.remove('hidden');
         document.getElementById('pause').classList.add('hidden');
       } else {
-        // Switch to break session
+        // Change to break session if a work session finished
         breakTime = true;
+        // Grab the break sessino time that the use wants
         sessionLength = document.getElementById('break-length').innerHTML;
 
+        // Display the break time
         $('#minutes').text(sessionLength);
         $('#seconds').text('00');
-        $('#notice').text('Break time!');
+        // Announce a break time will begin next
+        $('#notice').text('Break time');
 
+        // Show the start button & hide the pause button
         document.getElementById('start').classList.remove('hidden');
         document.getElementById('pause').classList.add('hidden');
       }
@@ -86,28 +101,36 @@
     function timerControl(el){
       if(el === 'start'){
 
+        // Start the timer
         timerBrain.countdown(sessionLength);
 
+        // Show the pause button & hide the start button
         document.getElementById('start').classList.add('hidden');
         document.getElementById('pause').classList.remove('hidden');
 
       } else if(el === 'pause'){
 
+        // Pause the timer
         timerBrain.pause();
 
+        // Show the start button & hide the pause button
         document.getElementById('start').classList.remove('hidden');
         document.getElementById('pause').classList.add('hidden');
 
       } else if(el === 'reset'){
 
+        // Set the timer as inactive
         active = false;
+        // Stop the timer if it's playing
         timerBrain.pause();
-
+        // Get the user's desired work session length
         sessionLength = document.getElementById('session-length').innerHTML;
 
+        // Display their designed work session length
         $('#minutes').text(sessionLength);
         $('#seconds').text('00');
 
+        // Display the start button & hide the pause button
         document.getElementById('start').classList.remove('hidden');
         document.getElementById('pause').classList.add('hidden');
 
@@ -115,28 +138,46 @@
     }
 
     function sessionControl(el){
+
+      // Isolating the user's adjusted time so that we can selectivly pass
+      // the timer length depending on if the work or break time is active
+      var tempLength = document.getElementById('session-length').innerHTML;
+
       if(el === 'decrease' && active === false){
 
-        if(sessionLength < 2){
-          sessionLength = 1;
+        // Don't allow the work session to go below 1 minute
+        if(tempLength < 2){
+          tempLength = 1;
+          // Don't display the work session time in the clock if we're on break time
+          if(!breakTime){
+            document.getElementById('minutes').innerHTML = tempLength;
+          }
           return;
         }
 
-        sessionLength--;
-        $('#session-length').text(sessionLength);
+        // Subtract one from the work session
+        tempLength--;
+        // Update the work session in the settings
+        $('#session-length').text(tempLength);
 
+        // Display the work session time in the clock if we're on a work session
         if(!breakTime){
-          document.getElementById('minutes').innerHTML = sessionLength;
+          document.getElementById('minutes').innerHTML = tempLength;
+          // Assign the user's adjusted work session time to the master session length
+          sessionLength = tempLength;
         }
 
       } else if(el === 'increase' && active === false){
 
-        var tempLength = document.getElementById('session-length').innerHTML;
+        // Add one to the work session
         tempLength++;
+        // Update the work session in the settings
         $('#session-length').text(tempLength);
 
+        // Display the work session time in the clock if we're on a work session
         if(!breakTime){
           document.getElementById('minutes').innerHTML = tempLength;
+          // Assign the user's adjusted work session time to our master session length
           sessionLength = tempLength;
         }
 
@@ -146,32 +187,42 @@
     function breakControl(el){
       if(el === 'decrease' && active === false){
 
+        // Don't allow the break session to go below 1 minute
         if(breakLength < 2){
           breakLength = 1;
           return;
         }
 
+        // Subtract one from the break session
         breakLength--;
+        // Update the break session in the settings
         $('#break-length').text(breakLength);
 
+        // Display the break session in the clock if we're on a break session
         if(breakTime){
-          sessionLength = breakLength;
           document.getElementById('minutes').innerHTML = sessionLength;
+          // Assign the user's adjusted break session time to the master session length
+          sessionLength = breakLength;
         }
 
       } else if(el === 'increase' && active === false){
 
+        // Increase one from the break session
         breakLength++;
+        // Update the break session in the settings
         $('#break-length').text(breakLength);
 
+        // Display the break session in the clock if we're on a break session
         if(breakTime){
-          sessionLength = breakLength;
           document.getElementById('minutes').innerHTML = sessionLength;
+          // Assign the user's adjusted break session time to the master session length
+          sessionLength = breakLength;
         }
 
       }
     }
 
+    // Handle each of our click events
     $('.action').click(function () {
       switch ( $(this).attr('id') ) {
         case 'start':
